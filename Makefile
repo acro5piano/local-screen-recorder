55ccdf0c87b174fc066282de0e97cbed710eec37
Kay Gosho <ketsume0211@gmail.com>
2022-05-12 23:19:36 +0900

    init

---
+++ Makefile
@@ -0,0 +1,11 @@
+dev:
+	make -j __serve __watch
+
+__serve:
+	php -S localhost:6677
+
+__watch:
+	yarn nodemon --watch src --ext ts --exec 'make build'
+
+build:
+	yarn --silent esbuild src/main.ts --bundle --minify > dist/main.min.js
---
+++ src/main.ts
@@ -0,0 +1,39 @@
+async function start() {
+  const audio = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+  })
+
+  const screen = await navigator.mediaDevices.getDisplayMedia({
+    audio: true,
+    video: true,
+  })
+
+  const stream = new MediaStream([...screen.getTracks(), ...audio.getTracks()])
+  const recorder = new MediaRecorder(stream)
+
+  const blobs: Blob[] = []
+
+  recorder.ondataavailable = (event) => {
+    if (!event.data.size) {
+      return
+    }
+    blobs.push(event.data)
+  }
+
+  recorder.start(1000)
+
+  async function end() {
+    recorder.stop()
+    const link = document.createElement('a')
+    const url = window.URL.createObjectURL(
+      new Blob(blobs, { type: 'video/webm' }),
+    )
+    link.download = `image-annotator-com-${new Date()}.webm`
+    link.href = url
+    link.click()
+  }
+
+  document.getElementById('end').addEventListener('click', end)
+}
+
+document.getElementById('start').addEventListener('click', start)
