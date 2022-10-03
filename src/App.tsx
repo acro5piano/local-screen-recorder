10f5b537a2c6e05c8858207248a2c8e01cca2b6e
Kay Gosho <ketsume0211@gmail.com>
2022-10-03 14:01:23 +0900

    some styling

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,25 @@ export const App = () => {
   }
 
   return (
-    <div className="">
-      {match(isStarted)
-        .with(true, () => <button onClick={onClickEnd}>End</button>)
-        .with(false, () => <button onClick={onClickStart}>Start</button>)
-        .exhaustive()}
+    <div className="flex justify-center items-center h-full bg-slate-800 text-slate-100">
+      <div className="w-96 h-96 border rounded p-8 flex flex-col justify-between">
+        <div className="flex justify-between">
+          <div className="text-2xl font-bold">Screen Recorder</div>
+        </div>
+        <div className="">
+          {match(isStarted)
+            .with(true, () => <Button onClick={onClickEnd}>End</Button>)
+            .with(false, () => <Button onClick={onClickStart}>Start</Button>)
+            .exhaustive()}
+        </div>
+      </div>
     </div>
   )
 }
+
+const Button = (props: React.ComponentProps<'button'>) => (
+  <button
+    className="bg-slate-100 text-slate-800 rounded px-4 py-1 shadow-md w-full font-bold"
+    {...props}
+  />
+)
