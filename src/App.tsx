24223eee71acf3b3b4f6753ffe587113dad3c88d
Kay Gosho <ketsume0211@gmail.com>
2022-10-03 13:48:46 +0900

    use ts-pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+import { match } from 'ts-pattern'
+import { startRecording } from './recorder'
 
 export const App = () => {
   const [isStarted, setIsStarted] = useState(false)
+  const endRecording = useRef<() => Promise<void>>()
+
+  const onClickStart = async () => {
+    setIsStarted(true)
+    const onEnd = await startRecording()
+    endRecording.current = onEnd
+  }
+
+  const onClickEnd = async () => {
+    setIsStarted(false)
+    await endRecording.current?.()
+  }
 
   return (
     <div className="">
-      <button id="start">start</button>
-      <button id="end">end</button>
+      {match(isStarted)
+        .with(true, () => <button onClick={onClickEnd}>End</button>)
+        .with(false, () => <button onClick={onClickStart}>Start</button>)
+        .exhaustive()}
     </div>
   )
 }
